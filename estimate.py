d3948e74ad746ebffee282f54140a4bb0118c930
Amer170202 <84259929+Amer170202@users.noreply.github.com>
2021-06-02 12:12:34 +0530

    Implemented functions

--- estimate.py
+++ estimate.py
@@ -1,5 +1,40 @@
 import math
 import unittest
+def wallis(x):
+    multiplier = 2
+    product = 1
+    for i in range(x):
+        term = multiplier**2 /(multiplier**2 - 1)
+        product *= term
+        multiplier += 2
+
+    return 2*product
+
+
+
+
+
+import random
+import math
+def monte_carlo(x):
+    circle_inside_points = 0
+    square_inside_points = 0
+    for i in range(x):
+        rand_x=random.uniform(-1,1)
+        rand_y=random.uniform(-1,1)
+        distance=math.sqrt(rand_x**2 + rand_y**2)
+
+
+        if(distance<1):
+            circle_inside_points += 1
+
+        square_inside_points += 1
+
+
+        pi= (4*circle_inside_points)/square_inside_points
+
+    return pi
+
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
@@ -30,3 +65,8 @@ class TestMC(unittest.TestCase):
     
 if __name__ == "__main__":
     unittest.main()
+
+
+    
+
+
